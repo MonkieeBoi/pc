efa0697c04231ca279535880125bf49610629598
MonkieeBoi <danny.pham26425@gmail.com>
2025-10-04 23:52:16 +1000

    feat: piece class

---
+++ js/piece.js
@@ -0,0 +1,20 @@
+class Piece {
+    constructor(type) {
+        this.x = Math.floor(BOARD_WIDTH / 2) - 1;
+        this.y = BOARD_HEIGHT - 1;
+        this.rotation = 0;
+        this.type = type;
+    }
+
+    get_minos() {
+        let minos = [];
+        for (let [off_x, off_y] of get_offsets(this.type, this.rotation)) {
+            minos.push([this.x + off_x, this.y + off_y]);
+        }
+        return minos;
+    }
+
+    get_colour() {
+        return get_colour(this.type);
+    }
+}
