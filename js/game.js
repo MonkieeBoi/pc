b7884391a5fbf11914d3c49610c4014c0a54b551
MonkieeBoi <danny.pham26425@gmail.com>
2025-10-06 22:07:41 +1100

    feat: game

--- js/game.js
+++ js/game.js
@@ -1,5 +1,37 @@
 class Game {
-	constructor() {
+    constructor() {
         this.board = new Board();
-	}
+        this.queue = new Queue();
+        this.active = this.queue.pop();
+        this.hold = 0;
+    }
+
+    check_collide(piece, cx, cy, rotation) {
+        for (let [off_x, off_y] of get_offsets(piece, rotation)) {
+            let x = cx + off_x;
+            let y = cy + off_y;
+            if (
+                x < 0 || x >= BOARD_WIDTH || y < 0 ||
+                this.board.is_filled(x, y)
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    get_ghost_y() {
+        let y = this.active.y;
+        while (
+            !this.check_collide(
+                this.active.type,
+                this.active.x,
+                y - 1,
+                this.active.rotation,
+            )
+        ) {
+            y--;
+        }
+        return y;
+    }
 }
